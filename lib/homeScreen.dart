955cf441585c2424e2eefb2a68bf466ea23d34c2
Aditya Shirsat <adityashirsat1170@gmail.com>
2026-04-17 10:57:46 +0530

    curd operation with state notifier provider

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,43 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_riverpod/legacy.dart';
-import 'package:riverpod_project/search_provider.dart';
+import 'package:riverpod_project/item_provider.dart';
 
 class Homescreen extends ConsumerWidget {
-  final countProvider = StateProvider<int>((ref) => 0);
-
   Homescreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final items = ref.watch(itemprovider);
     return Scaffold(
-      appBar: AppBar(title: Text("Riverpod Counter")),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextField(
-            onChanged: (value) {
-              ref.read(searchprovider.notifier).search(value);
-            },
-          ),
-          Consumer(
-            builder: (context, ref, child) {
-              final search = ref.watch(searchprovider);
-              return Text(search.search);
-            },
-          ),
-          Consumer(
-            builder: (context, ref, child) {
-              final search = ref.watch(searchprovider);
-              return Switch(
-                value: search.ischange,
-                onChanged: (value) {
-                  ref.read(searchprovider.notifier).onchange(value);
-                },
-              );
-            },
-          ),
-        ],
+      appBar: AppBar(title: Text("add list")),
+      body: items.isEmpty
+          ? Text("no data found ")
+          : ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                final itemdetails = items[index];
+                return ListTile(
+                  title: Text(itemdetails.name),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          ref
+                              .read(itemprovider.notifier)
+                              .updateItem(itemdetails.id, "update item");
+                        },
+                        icon: Icon(Icons.edit),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          ref
+                              .read(itemprovider.notifier)
+                              .deleteItem(itemdetails.id);
+                        },
+                        icon: Icon(Icons.delete),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ref.read(itemprovider.notifier).addItem("new item");
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
